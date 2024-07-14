1915ff04c8452b5c353b295351715786cedd7c27
Federico Maria Morrone <contact@morrone.dev>
2024-07-15 00:16:17 +0200

    Add prefetch strategy

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,9 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://verdi.rocks',
+  prefetch: {
+    prefetchAll: true,
+    defaultStrategy: 'load',
+  },
   integrations: [tailwind()],
 });
