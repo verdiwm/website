6c3eb8935c3b274f0d5f356990db83db99afcfc4
Federico Maria Morrone <contact@morrone.dev>
2024-08-02 11:40:25 +0200

    Add husky hooks

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,14 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
-import react from "@astrojs/react";
+import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://verdi.rocks',
   prefetch: {
     prefetchAll: true,
-    defaultStrategy: 'load'
+    defaultStrategy: 'load',
   },
-  integrations: [tailwind(), react()]
+  integrations: [tailwind(), react()],
 });
