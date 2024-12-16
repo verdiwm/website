2b6ae0f33f5f302a159cb7e3174224abd3fba5ac
Federico Maria Morrone <contact@morrone.dev>
2024-12-16 19:00:25 +0100

    Update react and astro template

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
+// @ts-check
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
-
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
